b14874555a5146ec6bcfd7924b81996ddb419234
cannero <serunmal@gmail.com>
2022-11-23 21:30:36 +0100

    serde default instead of option

--- src/sdk_style_reader.rs
+++ src/sdk_style_reader.rs
@@ -17,31 +17,29 @@ struct PackageReference {
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "PascalCase")]
 struct ItemGroup {
-    package_reference: Option<Vec<PackageReference>>,
+    #[serde(default)]
+    package_reference: Vec<PackageReference>,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "PascalCase")]
 struct Project {
-    item_group: Option<Vec<ItemGroup>>,
+    #[serde(default)]
+    item_group: Vec<ItemGroup>,
 }
 
 fn read_csproj<P: AsRef<Path>>(path: P) -> Project {
     let f = File::open(path).expect("csproj read");
     let b = BufReader::new(f);
 
-    from_reader(b).expect("check nodes without end")
+    from_reader(b).expect("check Project nodes")
 }
 
 pub fn add_packages<P: AsRef<Path>>(path: P, versioner: &mut Versioner){
     let project = read_csproj(path);
-    if let Some(item_groups) = project.item_group {
-        for item_group in item_groups {
-            if let Some(packages) = item_group.package_reference {
-                for package in packages {
-                    versioner.add(&package.include, &package.version);
-                }
-            }
+    for item_group in project.item_group {
+        for package in item_group.package_reference {
+            versioner.add(&package.include, &package.version);
         }
     }
 }
@@ -53,24 +51,24 @@ mod tests {
     #[test]
     fn parse_new_style() {
         let result = read_csproj("./tests/sdk_style.csproj");
-        let item_groups = result.item_group.unwrap();
+        let item_groups = result.item_group;
         assert_eq!(item_groups.len(), 3);
-        let package_ref = item_groups[0].package_reference.as_ref().unwrap();
+        let package_ref: &Vec<_> = &item_groups[0].package_reference;
         assert_eq!(package_ref.len(), 5);
     }
 
     #[test]
     fn parse_no_packages() {
         let result = read_csproj("./tests/sdk_style_no_package.csproj");
-        let item_groups = result.item_group.unwrap();
+        let item_groups = result.item_group;
         assert_eq!(item_groups.len(), 2);
-        assert!(item_groups[0].package_reference.is_none());
-        assert!(item_groups[1].package_reference.is_none());
+        assert!(item_groups[0].package_reference.is_empty());
+        assert!(item_groups[1].package_reference.is_empty());
     }
 
     #[test]
     fn parse_no_item_groups() {
         let result = read_csproj("./tests/sdk_style_no_item_group.csproj");
-        assert!(result.item_group.is_none());
+        assert!(result.item_group.is_empty());
     }
 }
